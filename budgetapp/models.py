3755bc70d0e6c2d1b7fbc91a518012ea9f543372
lingsin5234 <sinto.005234@gmail.com>
2020-01-05 17:17:35 -0500

    created test data model and view

--- budgetapp/models.py
+++ budgetapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class TestData(models.Model):
+    name = models.CharField(max_length=120)
+    amount = models.FloatField()
+    category = models.CharField(max_length=120)
--- budgetapp/views.py
+++ budgetapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from budgetapp.models import TestData
+
 
 # Create your views here.
+def upload_data(request):
+
+    budget_data = TestData.objects.all()
+
+    context = {
+        'budget_data': budget_data
+    }
+
+    return render(request, 'pages/upload.html', context)
