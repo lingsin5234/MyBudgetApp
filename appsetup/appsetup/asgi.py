2c1f408f0cece33598912980609b1a02a985e93d
lingsin5234 <sinto.005234@gmail.com>
2020-01-05 15:36:02 -0500

    start django project

--- appsetup/appsetup/asgi.py
+++ appsetup/appsetup/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for budgetapp project.
+ASGI config for appsetup project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appsetup.settings')
 
 application = get_asgi_application()
--- appsetup/appsetup/wsgi.py
+++ appsetup/appsetup/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for budgetapp project.
+WSGI config for appsetup project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appsetup.settings')
 
 application = get_wsgi_application()
