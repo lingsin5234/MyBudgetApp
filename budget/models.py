bdaee25e456fb4174b3526a6772502566a7443c5
lingsin5234 <sinto.005234@gmail.com>
2020-01-31 15:02:16 -0700

    LineItem setup

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class LineItem(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=20)
+    date_stamp = models.DateField()
+    amount = models.FloatField()
