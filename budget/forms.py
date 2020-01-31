316d69b28186c7e9d162f99ad3ed900b5297bae3
lingsin5234 <sinto.005234@gmail.com>
2020-01-31 16:44:40 -0700

    using ModelForm for upload form

--- budget/forms.py
+++ budget/forms.py
@@ -1,8 +1,8 @@
 from django import forms
+from .models import LineItem
 
 
-class UploadData(forms.Form):
-    line_item = forms.CharField(label='Line Item', max_length=30)
-    category = forms.CharField(label='Category', max_length=20)
-    date_stamp = forms.DateField(label='Date')
-    amount = forms.FloatField(label='Amount')
+class UploadDataForm(forms.ModelForm):
+    class Meta:
+        model = LineItem
+        fields = ['name', 'category', 'date_stamp', 'amount']
--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import LineItem
-from .forms import UploadData
+from .forms import UploadDataForm
 
 
 # Create your views here.
@@ -18,16 +18,16 @@ def upload_data(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = UploadData(request.POST)
+        form = UploadDataForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            
+            form.save()
             # redirect to a new URL:
             return HttpResponseRedirect('/thanks/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = UploadData()
+        form = UploadDataForm()
 
     return render(request, 'pages/upload.html', {'form': form})
