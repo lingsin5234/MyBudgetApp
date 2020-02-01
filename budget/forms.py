5ca73aff958c92e4f8db19b5eb0c2254d8ca1dbc
lingsin5234 <sinto.005234@gmail.com>
2020-02-01 11:37:35 -0700

    success with multiple data but same upload template

--- budget/forms.py
+++ budget/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import LineItem
+from .models import LineItem, Category
 
 
-class UploadDataForm(forms.ModelForm):
+class UploadLineItemForm(forms.ModelForm):
     class Meta:
         model = LineItem
         fields = ['name', 'category', 'date_stamp', 'amount']
+
+
+class UploadCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import LineItem
-from .forms import UploadDataForm
+from .forms import UploadLineItemForm, UploadCategoryForm
 
 
 # Create your views here.
@@ -14,21 +14,34 @@ def show_data(request):
     return render(request, 'pages/display.html', context)
 
 
-def upload_data(request):
+def upload_data(request, upload_type):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = UploadDataForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            form.save()
-            # redirect to a new URL:
-            return HttpResponseRedirect('/upload_done/')
+        if upload_type == 'line_item':
+            form = UploadLineItemForm(request.POST)
+
+            # check whether it's valid:
+            if form.is_valid():
+                # process the data in form.cleaned_data as required
+                form.save()
+                # redirect to a new URL:
+                return HttpResponseRedirect('/upload_done/')
+        else:
+            form = UploadCategoryForm(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                # process the data in form.cleaned_data as required
+                form.save()
+                # redirect to a new URL:
+                return HttpResponseRedirect('/upload_cat/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = UploadDataForm()
+        if upload_type == 'line_item':
+            form = UploadLineItemForm()
+        else:
+            form = UploadCategoryForm()
 
     return render(request, 'pages/upload.html', {'form': form})
 
