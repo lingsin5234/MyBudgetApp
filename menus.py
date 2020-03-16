97f3c4ad243534a95aab6ee8bd1d6d26c7306256
lingsin5234 <sinto.005234@gmail.com>
2020-03-16 14:23:38 -0600

    updated menu and display.html

--- menus.py
+++ menus.py
@@ -3,4 +3,4 @@ from django.urls import reverse
 from . import views as budget_vw
 
 # add items to the menu
-Menu.add_item("baseball", MenuItem("Home", reverse(budget_vw.show_d3), weight=10))
+Menu.add_item("budget", MenuItem("Home", reverse(budget_vw.show_d3), weight=10))
