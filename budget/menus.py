329f460faf01337f5311175ac0ef82f860e8fa8c
lingsin5234 <sinto.005234@gmail.com>
2020-08-21 08:14:25 -0600

    updated menu, project page, README

--- budget/menus.py
+++ budget/menus.py
@@ -4,7 +4,7 @@ from . import views as budget_vw
 
 # add items to the menu
 Menu.add_item("budget", MenuItem("My Portfolio", url="/", weight=10))
-Menu.add_item("budget", MenuItem("Budget Project", reverse(budget_vw.project_markdown), weight=10))
+Menu.add_item("budget", MenuItem("Project Info", reverse(budget_vw.project_markdown), weight=10))
 Menu.add_item("budget", MenuItem("Demo Dashboard", reverse(budget_vw.show_dashboard), weight=10))
 Menu.add_item("budget", MenuItem("Demo Plotly", reverse(budget_vw.show_plotly_dash), weight=10))
 Menu.add_item("budget", MenuItem("Demo Data", reverse(budget_vw.show_data), weight=10))
